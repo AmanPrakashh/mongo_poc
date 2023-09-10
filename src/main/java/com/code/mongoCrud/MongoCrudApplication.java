4054ab7e8a35bdcff8d1539b81c136283f87a77e
aman prakash <amanjhasahab@gmail.com>
2023-09-10 12:24:57 +0530

    ADDED:: new mongo properties

--- src/main/java/com/code/mongoCrud/MongoCrudApplication.java
+++ src/main/java/com/code/mongoCrud/MongoCrudApplication.java
@@ -2,10 +2,10 @@ package com.code.mongoCrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableSwagger2
 public class MongoCrudApplication {
 
 	public static void main(String[] args) {
