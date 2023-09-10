ac41339769a2f69bc439bd187d50f6eeb5467049
aman prakash <amanjhasahab@gmail.com>
2023-09-10 12:27:24 +0530

    ADDED:: get , delete and search person api

--- src/main/java/com/code/mongoCrud/collection/Person.java
+++ src/main/java/com/code/mongoCrud/collection/Person.java
@@ -3,6 +3,7 @@ package com.code.mongoCrud.collection;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,7 +16,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Person {
     @Id
-    private String personId;
+    private Integer personId;
     private String firstName;
     private String lastName;
     private Integer age;
